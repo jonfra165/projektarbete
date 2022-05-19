903cff81bc0b81646d70ce193344fd3f7a2bf6d9
jonfra165 <jonna.franzen@hotmail.com>
2022-05-19 14:50:20 +0200

    Uppdaterat api-anropen, tagit bort överflödig kod.

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Spinner from "./Spinner";
 
-function Giphy() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+const Giphy = () => {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-    
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       setIsError(false);
       setIsLoading(true);
 
       try {
-        const giphyResults = await axios("https://api.giphy.com/v1/gifs/trending", {
+        const results = await axios("https://api.giphy.com/v1/gifs/trending", {
           params: {
             api_key: "Kt88WlJH3B83KOdKYnWKcEW1oX6sICUk",
-            limit: 25
+            limit: 10
           }
         });
-        console.log(giphyResults);
-        setData(giphyResults.data.data);
+        console.log(results);
+        setData(results.data.data);
       } catch (err) {
         setIsError(true);
         setTimeout(() => setIsError(false), 4000);
@@ -32,13 +33,82 @@ function Giphy() {
     fetchData();
   }, []);
 
-    return (
-      <div>
-          <h1>Giffa loss!</h1>
-          <span>Livet blir roligare med gifs</span>
-      </div>
-    );
-  }
-  
-  export default Giphy;
-  
+  const renderGifs = () => {
+    if (isLoading) {
+      <Spinner />;
+    }
+    return data.map(el => {
+      return (
+        <div key={el.id}>
+          <img alt="gif" src={el.images.fixed_height.url} />
+        </div>
+      );
+    });
+  };
+  const renderError = () => {
+    if (isError) {
+      return (
+        <div
+          className="alert alert-danger alert-dismissible fade show"
+          role="alert"
+        >
+          Kunde inte hämta gif.
+        </div>
+      );
+    }
+  };
+
+  const handleSearchChange = event => {
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    setIsError(false);
+    setIsLoading(true);
+
+    try {
+      const results = await axios("https://api.giphy.com/v1/gifs/search", {
+        params: {
+          api_key: "Kt88WlJH3B83KOdKYnWKcEW1oX6sICUk",
+          q: search,
+          limit: 50
+        }
+      });
+      setData(results.data.data);
+    } catch (err) {
+      setIsError(true);
+      setTimeout(() => setIsError(false), 4000);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="m-2">
+      {renderError()}
+      <form className="form-inline justify-content-center m-2">
+        <input
+          value={search}
+          onChange={handleSearchChange}
+          type="text"
+          placeholder="search"
+          className="form-control"
+        />
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="btn btn-primary mx-2btn btn-primary btn-sm"
+        >
+          Sök
+        </button>
+      </form>
+      
+      <div className="card" style={{width: "18rem"}}>{renderGifs()}</div>
+      
+      
+    </div>
+  );
+};
+
+export default Giphy;
