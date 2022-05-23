9b8b570ccfc559015a3a7b52ca4a8c7c2eac2f42
jonfra165 <jonna.franzen@hotmail.com>
2022-05-23 14:31:36 +0200

    Bröt ut Gif som en egen komponent.
    Co-Authored-By: juliaahrling <81237729+juliaahrling@users.noreply.github.com>
    Co-Authored-By: wongren <81237890+wongren@users.noreply.github.com>

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "./Spinner";
+import Gif from "./Gif";
 
 const Giphy = () => {
   const [data, setData] = useState([]);
@@ -39,9 +40,7 @@ const Giphy = () => {
     }
     return data.map(el => {
       return (
-        <div key={el.id}>
-          <img alt="gif" src={el.images.fixed_height.url} />
-        </div>
+        <Gif id={el.id} url={el.images.fixed_height.url}/>
       );
     });
   };
