7adc99da271ea63c8bb5045be201a067ab4ba046
wongren <81237890+wongren@users.noreply.github.com>
2022-05-24 15:05:51 +0200

    lagt till html/css dagbok

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Giphy from './components/Giphy';
 import Navbar from './components/Navbar';
+import Diary from './components/Diary';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Giphy />
+      <Diary />
     </>
   );
 }
---
+++ src/components/Diary.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Diary = () => {
+    return (
+        <div class="container">
+            <h1 className='fw-bold mt-5'>Dagbok</h1>
+            <div class="row">
+            <div class="card mt-2" style={{width: '10rem'}} >
+                <img src="https://i.giphy.com/media/oJtNvTyM11S4MMB3Mk/200.gif" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">24-05-2022</p>
+                </div>
+            </div>
+            <div class="card mt-2" style={{width: '10rem'}} >
+                <img src="https://i.giphy.com/media/oJtNvTyM11S4MMB3Mk/200.gif" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">24-05-2022</p>
+                </div>
+            </div>
+            <div class="card mt-2" style={{width: '10rem'}} >
+                <img src="https://i.giphy.com/media/oJtNvTyM11S4MMB3Mk/200.gif" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">24-05-2022</p>
+                </div>
+            </div>
+            <div class="card mt-2" style={{width: '10rem'}} >
+                <img src="https://i.giphy.com/media/oJtNvTyM11S4MMB3Mk/200.gif" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">24-05-2022</p>
+                </div>
+            </div>
+            <div class="card mt-2" style={{width: '10rem'}} >
+                <img src="https://i.giphy.com/media/oJtNvTyM11S4MMB3Mk/200.gif" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">24-05-2022</p>
+                </div>
+            </div>
+            <div class="card mt-2" style={{width: '10rem'}} >
+                <img src="https://i.giphy.com/media/oJtNvTyM11S4MMB3Mk/200.gif" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">24-05-2022</p>
+                </div>
+            </div>
+            <div class="card mt-2" style={{width: '10rem'}} >
+                <img src="https://i.giphy.com/media/oJtNvTyM11S4MMB3Mk/200.gif" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">24-05-2022</p>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default Diary;
