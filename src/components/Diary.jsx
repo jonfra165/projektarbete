fcc9d95aead6656da12bf1c274fc29c7871707fa
jonfra165 <jonna.franzen@hotmail.com>
2022-05-27 17:10:22 +0200

    Update Diary.jsx

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -5,18 +5,6 @@ const Diary = (props) => {
     return (
         <>
             <h1 className='fw-bold mt-5'>Dagbok</h1>
-<<<<<<< HEAD
-            <div className="row">
-             {props.gifList.map( el => {
-                 return (
-                    <div className="card mt-2" style={{width: '10rem'}} >
-                    <Gif id={el.id} url={el.url} />
-                    <div className="card-body">
-                        <p className="card-text">{el.date}</p>
-                    </div>
-                </div>
-                 )
-=======
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4 text-center d-flex align-items-stretch mb-3">
                 {props.gifList.map(el => {
                     return (
@@ -31,7 +19,6 @@ const Diary = (props) => {
                             </div>
                         </div>
                     )
->>>>>>> feature_giphy
                 }
                 )}
             </div>
