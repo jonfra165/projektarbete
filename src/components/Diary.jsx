0eaf32193ba028f11a35ed1a8a1d8d2de0b1559d
jonfra165 <jonna.franzen@hotmail.com>
2022-05-27 16:52:51 +0200

    Indenterade koden

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -6,22 +6,21 @@ const Diary = (props) => {
         <>
             <h1 className='fw-bold mt-5'>Dagbok</h1>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4 text-center d-flex align-items-stretch mb-3">
-             {props.gifList.map( el => {
-                 return (
-
-                    <div className="col">
-                    <div className="card">
-                        <Gif id={el.id} url={el.url} />
-                        <div class="card-body d-flex flex-column">
-                            <div className="card-text">
-                                2022-05-30
+                {props.gifList.map(el => {
+                    return (
+                        <div className="col">
+                            <div className="card">
+                                <Gif id={el.id} url={el.url} />
+                                <div class="card-body d-flex flex-column">
+                                    <div className="card-text">
+                                        2022-05-30
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                 )
+                    )
                 }
-             )}
+                )}
             </div>
         </>
     );
