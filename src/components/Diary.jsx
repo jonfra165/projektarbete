b06f6fb2b80bcc993503883f960f622718e00b33
jonfra165 <jonna.franzen@hotmail.com>
2022-05-29 21:44:23 +0200

    Bootstrap
    Ändrade bootstrapkod på rad 7 och la till bootstrap på rad 6.
    
    Tog bort logo och manifest i index.html som gav felmeddelande i consolen.

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -3,9 +3,9 @@ import Gif from "./Gif";
 
 const Diary = (props) => {
     return (
-        <>
-            <h1 className='fw-bold mt-5' id="scrollspyDiary">Dagbok</h1>
-            <div className="row row-cols-2 row-cols-sm-4 row-cols-md-6 g-4 text-center d-flex align-items-stretch mb-3">
+        <div className="container bg-light border rounded-3">
+            <h1 className='fw-bold mt-2' id="scrollspyDiary">Dagbok</h1>
+            <div className="row row-cols-2 row-cols-sm-4 row-cols-md-4 row-cols-lg-6 g-4 text-center d-flex align-items-stretch mb-3">
                 {props.gifList.map((el, i) => {
                     return (
                         <div key={i} className="col">
@@ -22,7 +22,7 @@ const Diary = (props) => {
                 }
                 )}
             </div>
-        </>
+        </div>
     );
 };
 
