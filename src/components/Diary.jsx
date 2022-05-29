3eb62e5416ecffaaaac8cf12fe192e9e7ad46b63
juliaahrling <juliaahrling@gmail.com>
2022-05-29 19:40:46 +0200

    Changed navbar + added scrollspy
    Changed design on navbar, added scrollspy on "Dagbok" in navbar so when klicked it scrolls down to the heading "Dagbok"

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -4,14 +4,14 @@ import Gif from "./Gif";
 const Diary = (props) => {
     return (
         <>
-            <h1 className='fw-bold mt-5'>Dagbok</h1>
+            <h1 className='fw-bold mt-5' id="scrollspyDiary">Dagbok</h1>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4 text-center d-flex align-items-stretch mb-3">
                 {props.gifList.map(el => {
                     return (
                         <div className="col">
                             <div className="card">
                                 <Gif id={el.id} url={el.url} />
-                                <div class="card-body d-flex flex-column">
+                                <div className="card-body d-flex flex-column">
                                     <div className="card-text">
                                         {new Date(el.date).toLocaleDateString()}
                                     </div>
