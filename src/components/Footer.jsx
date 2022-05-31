b1e7e1816f7a094499598c0d4f0a6ba8a0fb559d
jonfra165 <jonna.franzen@hotmail.com>
2022-05-31 22:12:00 +0200

    Glömde uppdatera text, rad 7

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="container">
         <footer className="py-3 my-4 border-top">
-          <p className="text-center text-muted">&copy; 2021 Company, Inc</p>
+          <p className="text-center text-muted">&copy; 2021 GIF-dagboken, Inc</p>
         </footer>
       </div>
     );
